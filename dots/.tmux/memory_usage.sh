e57d92b4dce8cdf5a6975e040348faf9392bad5e
LFDM <1986gh@gmail.com>
2025-11-15 22:16:56 +0100

    Mem and CPU usage

--- dots/.tmux/memory_usage.sh
+++ dots/.tmux/memory_usage.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mem_info=$(awk '
+mem_pct=$(awk '
 /MemTotal:/ { total = $2 }
 /MemAvailable:/ { available = $2 }
 END {
-  used = total - available
-  printf "%.1fG/%.1fG %2.0f%%\n", used / 1048576, total / 1048576, (used / total) * 100
+  if (total == 0) {
+    print "0"
+    exit
+  }
+  printf "%.0f", ((total - available) / total) * 100
 }
 ' /proc/meminfo)
 
-printf "%s" "$mem_info"
+read -r _ user nice system idle iowait irq softirq steal guest guest_nice < /proc/stat
+prev_total=$((user + nice + system + idle + iowait + irq + softirq + steal))
+prev_idle=$((idle + iowait))
+
+sleep 0.2
+
+read -r _ user nice system idle iowait irq softirq steal guest guest_nice < /proc/stat
+total=$((user + nice + system + idle + iowait + irq + softirq + steal))
+idle_total=$((idle + iowait))
+
+delta_total=$((total - prev_total))
+delta_idle=$((idle_total - prev_idle))
+
+cpu_pct=$(awk -v total="$delta_total" -v idle="$delta_idle" '
+BEGIN {
+  if (total <= 0) {
+    print "0"
+    exit
+  }
+  printf "%.0f", (1 - (idle / total)) * 100
+}')
+
+printf "Mem %s%% CPU %s%%" "$mem_pct" "$cpu_pct"
